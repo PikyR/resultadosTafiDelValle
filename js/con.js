8ef5211f893901f714d62ac2e0926a8ea8d021b0
pikyR <pikytdv@gmail.com>
2023-05-29 22:41:25 -0300

    Iniciando pantalla Concejales

--- js/con.js
+++ js/con.js
@@ -1,16 +1,24 @@
-// LEGISLADOR
-const cardsContainerLeg = document.querySelector(".cards-container--leg");
-const mesasEscrutadasLeg = document.querySelector("#mesas-escrutadas-leg");
-const votosEmitidosLeg = document.querySelector("#votos-emitidos-leg");
-const votosBlancoLeg = document.querySelector("#votos-blanco-leg");
+// GOBERNADOR
+const cardsContainerCon = document.querySelector(".cards-container--conc");
+const cardsContainerConPrimary = document.querySelector(".cards-container--primary");
+const mesasEscrutadasCon = document.querySelector("#mesas-escrutadas-conc");
+const votosEmitidosCon = document.querySelector("#votos-emitidos-conc");
+const votosBlancoCon = document.querySelector("#votos-blanco-conc");
 
-function renderLeg(data, i) {
+function renderCon(data, i) {
   const card = document.createElement('div');
   card.classList.add('card');
-  card.classList.add('card--legislador');
+  card.classList.add('card--gobernador');
 
   const cardHeader = document.createElement('div');
   cardHeader.classList.add('card__header');
+
+  const cardImage = document.createElement('img');
+  cardImage.classList.add('card__image');
+  // CAMBIAR                              
+  // cardImage.setAttribute('src', `assets/images/gobernador/${data.LISTA}.png`);
+  cardImage.setAttribute('src', `assets/images/gobernador/img.jpg`);
+  cardImage.setAttribute('alt', `${data.CANDIDATO}`);
 
   const cardInfo = document.createElement('div');
   cardInfo.classList.add('card__info');
@@ -34,7 +42,13 @@ function renderLeg(data, i) {
   candidato.textContent = `${data.CANDIDATO}`;
 
   cardInfo.append(infoText, lista, candidato);
-  cardHeader.append(cardInfo);
+
+  if (i <= 5) {
+    card.classList.add('card--primary');
+    cardHeader.append(cardImage,cardInfo);
+  } else {
+    cardHeader.append(cardInfo);
+  }
 
   const cardBody = document.createElement('div');
   cardBody.classList.add('card__body');
@@ -50,22 +64,27 @@ function renderLeg(data, i) {
 
   card.append(cardHeader, cardBody);
 
-  cardsContainerLeg.appendChild(card);
+  if (i <= 5) {
+    cardsContainerConPrimary.appendChild(card);
+  } else {
+    cardsContainerCon.appendChild(card);
+  }
 }
 
-async function loadLeg() {
-  const resultadoLeg = await fetchData(URL(CATEGORIA.LEG));
-  const resultadoDividido = dividirArreglo(resultadoLeg);
+async function loadCon() {
+  const resultadoCon = await fetchData(URL(CATEGORIA.CON));
+  const resultadoDividido = dividirArreglo(resultadoCon);
 
   const resultadoCandidatos = ordenarPorVotos(resultadoDividido[0]);
+  console.log('concejales', resultadoCandidatos);
   const resultadoOtros = ordenarPorVotos(resultadoDividido[1]);
 
-  votosEmitidosLeg.textContent = `${resultadoOtros[0].VOTOS}`;
-  votosBlancoLeg.textContent = `${resultadoOtros[1].VOTOS}`;
+  votosEmitidosCon.textContent = `${resultadoOtros[0].VOTOS}`;
+  votosBlancoCon.textContent = `${resultadoOtros[1].VOTOS}`;
 
   resultadoCandidatos.forEach((element, i) => {
-    renderLeg(element, i);
+    renderCon(element, i);
   });
 }
 
-loadLeg();
+loadCon();
